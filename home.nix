45fdb6b0669358d3cf8b5b660fd91ff10efdd720
luxqw <rakhmatullin.damir@tutamail.com>
2026-06-27 22:38:03 +0200

    fix: replace foot xdg.configFile with programs.foot to avoid pure eval store path error

--- home.nix
+++ home.nix
@@ -64,5 +64,38 @@
     };
   };
 
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "Iosevka NF:size=12";
+        pad  = "4x4";
+      };
+      colors-dark = {
+        foreground = "c0caf5";
+        background = "1a1b26";
+        alpha      = "0.98";
+        regular0   = "15161E";
+        regular1   = "f7768e";
+        regular2   = "9ece6a";
+        regular3   = "e0af68";
+        regular4   = "7aa2f7";
+        regular5   = "bb9af7";
+        regular6   = "7dcfff";
+        regular7   = "a9b1d6";
+        bright0    = "414868";
+        bright1    = "f7768e";
+        bright2    = "9ece6a";
+        bright3    = "e0af68";
+        bright4    = "7aa2f7";
+        bright5    = "bb9af7";
+        bright6    = "7dcfff";
+        bright7    = "c0caf5";
+        dim0       = "ff9e64";
+        dim1       = "db4b4b";
+      };
+    };
+  };
+
   programs.home-manager.enable = true;
 }
